81f8ea59dbedaaaf6312249c0dc8a61219916bf8
SDSirius <114844732+SDSirius@users.noreply.github.com>
2023-04-10 15:30:21 -0300

    Update cadastro.ts

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -11,39 +11,43 @@ import { politicaCORS } from "../../MIddlewares/politicaCORS";
 const handler = nc()
     .use(upload.single('file'))
     .post(async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMSG>) => {
+        try{
+            const usuario = req.body as CadastroRequisicao;
 
-        const usuario = req.body as CadastroRequisicao;
+            if(!usuario.nome || usuario.nome.length < 2){
+                return res.status(400).json({erro : 'Nome Inválido'});
+            }
+            if(!usuario.email || usuario.email.length < 5
+                || !usuario.email.includes('@')
+                || !usuario.email.includes('.')){
+                return res.status(400).json({erro : 'E-mail Inválido'});
+            }
+            if(!usuario.senha || usuario.senha.length < 4){
+                return res.status(400).json({erro : 'Senha Inválida'});
+            }
+            
+            const userSameEmail = await UserModel.find({email : usuario.email});
+            if(userSameEmail && userSameEmail.length > 0){
+                return res.status(400).json({erro : 'E-mail ja cadastrado!'})
+            }
+            const image = await uploadImagesCosmic(req);
 
-        if(!usuario.nome || usuario.nome.length < 2){
-            return res.status(400).json({erro : 'Nome Inválido'});
-        }
-        if(!usuario.email || usuario.email.length < 5
-            || !usuario.email.includes('@')
-            || !usuario.email.includes('.')){
-            return res.status(400).json({erro : 'E-mail Inválido'});
-        }
-        if(!usuario.senha || usuario.senha.length < 8){
-            return res.status(400).json({erro : 'Senha Inválida'});
-        }
-        
-        const image = await uploadImagesCosmic(req);
+            const userToBeSaved = {
+                nome : usuario.nome,
+                email : usuario.email,
+                senha : md5(usuario.senha),
+                avatar : image?.media?.url
+            }
 
-        const userToBeSaved = {
-            nome : usuario.nome,
-            email : usuario.email,
-            senha : md5(usuario.senha),
-            avatar : image.media.url
-        }
 
-        const userSameEmail = await UserModel.find({email : usuario.email});
-        if(userSameEmail && userSameEmail.length > 0){
-            return res.status(400).json({erro : 'E-mail ja cadastrado!'})
-        }
-
-        await UserModel.create(userToBeSaved);
-        return res.status(200).json({msg : 'Usuário criado com sucesso!'});
+            await UserModel.create(userToBeSaved);
+            return res.status(200).json({msg : 'Usuário criado com sucesso!'});
+    }catch(e:any){
+        console.log(e);
+        return res.status(400).json({erro : e.toString()});
+    }
     
-})
+});
 
 export const config ={
     api:{
